3269681206c9b0cd1b39bbc6c922a67b8517aac0
Tooomas <tomek.dominiczak@gmail.com>
2022-07-05 11:54:53 +0200

    Add wishlist hook

--- src/react/hooks.ts
+++ src/react/hooks.ts
@@ -3,3 +3,4 @@ import { hookFactory } from "./useHook";
 export const useAuth = hookFactory("auth");
 export const useCheckout = hookFactory("checkout");
 export const useCart = hookFactory("cart");
+export const useWishlist = hookFactory("wishlist");
