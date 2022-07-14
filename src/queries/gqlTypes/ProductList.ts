2b491d3cb3e8538d6f50e5cb5f163708610a961a
Tooomas <tomek.dominiczak@gmail.com>
2022-07-14 11:00:04 +0200

    Fix types

--- src/queries/gqlTypes/ProductList.ts
+++ src/queries/gqlTypes/ProductList.ts
@@ -243,7 +243,7 @@ export interface ProductList_products_edges_node {
    * Lists the storefront product's pricing, the current price and discounts, only meant for displaying.
    */
   pricing: ProductList_products_edges_node_pricing | null;
-  collections: ProductList_products_edges_node_collections[] | null;
+  collections?: ProductList_products_edges_node_collections[] | null;
 }
 
 export interface ProductList_products_edges_node_collections {
