9f3e9b5ba67aea9ddc29877c59df97724bda71ff
IgorSwatowski <igi66@wp.pl>
2022-09-21 08:35:57 +0200

    Expand query for sale

--- src/queries/sales.ts
+++ src/queries/sales.ts
@@ -4,6 +4,7 @@ import {
   saleFragment,
 } from "../fragments/sales";
 import { pageInfo } from "../fragments/pageInfo";
+import { baseProductFragment, productPricingFragment } from "src/fragments/products";
 
 export const sales = gql`
   ${baseSaleFragment}
@@ -37,9 +38,29 @@ export const sales = gql`
 
 export const saleDetails = gql`
   ${saleFragment}
+  ${baseProductFragment}
+  ${productPricingFragment}
+  ${pageInfo}
   query SaleDetails($id: ID!, $channel: String) {
     sale(id: $id, channel: $channel) {
       ...SaleDetails
+      products(
+        after: $after
+        first: $first
+        sortBy: $sortBy
+        filter: $filter
+        channel: $channel
+      ) {
+        edges {
+          node {
+            ...BaseProduct
+            ...ProductPricingField
+          }
+        }
+        pageInfo {
+          ...PageInfo
+        }
+      }
     }
   }
 `;
