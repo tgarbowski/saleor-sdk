cfb0804e8d3d719119755bcc6e00a3447ce88ac8
IgorSwatowski <igi66@wp.pl>
2022-09-21 08:55:05 +0200

    Fix sales

--- src/queries/sales.ts
+++ src/queries/sales.ts
@@ -4,7 +4,6 @@ import {
   saleFragment,
 } from "../fragments/sales";
 import { pageInfo } from "../fragments/pageInfo";
-import { baseProductFragment, productPricingFragment } from "src/fragments/products";
 
 export const sales = gql`
   ${baseSaleFragment}
@@ -38,29 +37,9 @@ export const sales = gql`
 
 export const saleDetails = gql`
   ${saleFragment}
-  ${baseProductFragment}
-  ${productPricingFragment}
-  ${pageInfo}
   query SaleDetails($id: ID!, $channel: String) {
     sale(id: $id, channel: $channel) {
       ...SaleDetails
-      products(
-        after: $after
-        first: $first
-        sortBy: $sortBy
-        filter: $filter
-        channel: $channel
-      ) {
-        edges {
-          node {
-            ...BaseProduct
-            ...ProductPricingField
-          }
-        }
-        pageInfo {
-          ...PageInfo
-        }
-      }
     }
   }
 `;
