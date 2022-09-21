681afa659a6d7fb92fbd2c7bed2fee45eb1a2162
IgorSwatowski <igi66@wp.pl>
2022-09-21 13:10:41 +0200

    Fix sales

--- src/queries/gqlTypes/SaleList.ts
+++ src/queries/gqlTypes/SaleList.ts
@@ -3,7 +3,7 @@
 // @generated
 // This file was automatically generated and should not be edited.
 
-import { SaleFilterInput, SaleSortingInput } from "src/gqlTypes/globalTypes";
+import { SaleSortingInput } from "src/gqlTypes/globalTypes";
 
 // ====================================================
 // GraphQL query operation: SaleList
@@ -62,6 +62,5 @@ export interface SaleListVariables {
   first: number;
   after?: string | null;
   sortBy?: SaleSortingInput | null;
-  filter?: SaleFilterInput | null;
   channel?: string | null;
 }
